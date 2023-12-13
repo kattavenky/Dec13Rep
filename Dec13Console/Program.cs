592dfa2902a869187d5b63154ebd7d15002967ad
abc <abc@gmail.com>
2023-12-13 12:23:49 +0530

    Add project files.

---
+++ Dec13Console/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;//namespace
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dec13Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whats up");//class.method
+            Console.ReadLine();
+        }
+    }// how to execute of result
+    //press ctrl +f5
+}
